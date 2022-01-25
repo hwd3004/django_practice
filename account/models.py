61238c0e430cf68c8c4f93b7388db14655734351
hwd3004 <hwd3004@gmail.com>
2022-01-25 16:49:13 +0900

    sign up

--- account/models.py
+++ account/models.py
@@ -7,5 +7,9 @@ class User(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self
+    # def __str__(self):
+    #     return self.userId
+
+    # return을 그냥 self로 하면 User.objects.get 같은 함수들이 작동하지 않는다
+    # def __str__(self):
+    #     return self
--- account/views.py
+++ account/views.py
@@ -1,28 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-from django.template import context
+import json
+from django.shortcuts import redirect, render
+from django.http import HttpRequest, JsonResponse
 
 from account.models import User
 
 
 def signup(request: HttpRequest):
+
     if(request.method == 'POST'):
-        userId = request.POST.get('userId')
-        password = request.POST.get('password')
+        data = json.loads(request.body)
 
-        newUser = User()
+        # https://my-devblog.tistory.com/28
+        # json과 dict 간에 서로 변환 방법
 
-        newUser.userId = userId
-        newUser.password = password
+        # https://link2me.tistory.com/2103
+        # https://link2me.tistory.com/2108
+        # django와 ajax 활용
 
-        # db에 인서트
-        newUser.save()
+        userId = data['userId']
+        password = data['password']
 
-        return render(request, 'signup.html')
+        # 0 - 이미 있는 아이디, 1 - 없는 아디, 회원가입 진행
+        result = {
+            'status': 0
+        }
+
+        try:
+            checkExistId = User.objects.get(userId=userId)
+
+            if checkExistId:
+                return JsonResponse(result)
+
+        except User.DoesNotExist:
+            newUser = User()
+
+            newUser.userId = userId
+            newUser.password = password
+            newUser.save()
+
+            result['status'] = 1
+
+            # ajax 사용시 JsonResponse를 리턴해야함
+            return JsonResponse(result)
+
     else:
         return render(request, 'signup.html')
 
 
-def login(request):
-    context = {'data': 'data'}
-    return render(request, 'login.html', context)
+def login(request: HttpRequest):
+    return render(request, 'login.html')
